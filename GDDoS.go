094a00ec22fa22102b210ca21c93ee65196f0c44
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-04-19 11:36:46 +0800

    add: Add more UA to fxxk the server.

--- GDDoS.go
+++ GDDoS.go
@@ -33,14 +33,19 @@ var (
 		},
 	}
 	UserAgents = []string{
-		"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0",
+		"Baiduspider-image+(+http://www.baidu.com/search/spider.htm)",
+		"Baiduspider-render/2.0; (+http://www.baidu.com/search/spider.html)",
 		"Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11",
 		"Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
+		"Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14",
+		"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)",
+		"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)",
 		"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)",
 		"Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
-		"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)",
 		"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
-		"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)",
+		"Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)",
+		"Mozilla/5.0 (compatible; Googlebot-Image/1.0; +http://www.google.com/bot.html)",
+		"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0",
 		"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
 	}
 
