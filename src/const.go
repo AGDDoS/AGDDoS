1c7eefb7bda1909b07dd0752bf9ea81b4f0b2cab
甜力怕 <pig2333minecraft@yeah.net>
2022-05-10 23:52:43 +0800

    [feat] 更改项目结构
    closes #10

---
+++ src/const.go
@@ -0,0 +1,63 @@
+package main
+
+// 定义各种常量、变量等
+const (
+	WelcomeMsg = "" +
+		"+----------------------------------------------------------------+ \n" +
+		"| Welcome to use AGDDOS                                          | \n" +
+		"| Code by AGDDoS Team                                            | \n" +
+		"| If you have some problem when you use the tool,                | \n" +
+		"| please submit issue at : https://github.com/AGDDoS/AGDDoS      | \n" +
+		"+----------------------------------------------------------------+"
+	HelpMsg = "" +
+		"---------- AGDDoS Help ----------\n" +
+		"[!]This argument was deprecated, please use following command:\n" +
+		"> AGDDoS -h\n\n" +
+		"[*]If you have some problem when you use the tool,\n" +
+		"please submit issue at : https://github.com/AGDDoS/AGDDoS"
+)
+
+var (
+	Total        = 0
+	SuccessTotal = 0
+	ErrorTotal   = 0
+	DisplayTotal bool
+	UserAgents   = []string{
+		"Baiduspider-image+(+http://www.baidu.com/search/spider.htm)",
+		"Baiduspider-render/2.0; (+http://www.baidu.com/search/spider.html)",
+		"Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11",
+		"Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
+		"Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14",
+		"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)",
+		"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)",
+		"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)",
+		"Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+		"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+		"Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)",
+		"Mozilla/5.0 (compatible; Googlebot-Image/1.0; +http://www.google.com/bot.html)",
+		"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0",
+		"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+		"AGDDoS/" + version + "(" + timestamp + ")",
+	}
+
+	Refs = []string{
+		"https://www.google.com/search?q=",
+		"https://check-host.net/",
+		"https://www.facebook.com/",
+		"https://www.youtube.com/",
+		"https://www.ip138.com/",
+		"https://www.bing.com/search?q=",
+		"https://r.search.yahoo.com/",
+		"https://www.china.gov/index.html",
+		"https://vk.com/profile.php?redirect=",
+		"https://blog.csdn.net/",
+		"https://help.baidu.com/searchResult?keywords=",
+		"https://steamcommunity.com/market/search?q=",
+		"https://wx.zsxq.com/",
+		"https://www.android-ide.com/tutorial_androidapp.html",
+		"https://www.baidu.com/s?ie=utf-8&wd=AGDDoS%2FAGDDoS",
+		"https://www.baidu.com/s?ie=utf-8&wd=github.com%2FAGDDoS%2FAGDDoS",
+		// Don't forget to add ourselves!
+		"https://github.com/AGDDoS/AGDDoS",
+	}
+)
--- src/main.go
+++ src/main.go
@@ -8,26 +8,9 @@ import (
 	"math/rand"
 	"net"
 	"net/http"
-	"os"
 	"runtime"
 	"strings"
 	"time"
-)
-
-const (
-	WelcomeMsg = "" +
-		"+----------------------------------------------------------------+ \n" +
-		"| Welcome to use AGDDOS                                          | \n" +
-		"| Code by AGDDoS Team                                            | \n" +
-		"| If you have some problem when you use the tool,                | \n" +
-		"| please submit issue at : https://github.com/AGDDoS/AGDDoS      | \n" +
-		"+----------------------------------------------------------------+"
-	HelpMsg = "" +
-		"---------- AGDDoS Help ----------\n" +
-		"[!]This argument was deprecated, please use following command:\n" +
-		"> AGDDoS -h\n\n" +
-		"[*]If you have some problem when you use the tool,\n" +
-		"please submit issue at : https://github.com/AGDDoS/AGDDoS"
 )
 
 // X args
@@ -35,7 +18,6 @@ var (
 	timestamp = "unknown"
 	version   = "unknown"
 )
-
 var (
 	Method              string
 	TargetUrl           string
@@ -55,55 +37,14 @@ var (
 			},
 		},
 	}
-	UserAgents = []string{
-		"Baiduspider-image+(+http://www.baidu.com/search/spider.htm)",
-		"Baiduspider-render/2.0; (+http://www.baidu.com/search/spider.html)",
-		"Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11",
-		"Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
-		"Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14",
-		"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)",
-		"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)",
-		"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)",
-		"Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
-		"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
-		"Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)",
-		"Mozilla/5.0 (compatible; Googlebot-Image/1.0; +http://www.google.com/bot.html)",
-		"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0",
-		"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
-		"AGDDoS/" + version + "(" + timestamp + ")",
-	}
-
-	Refs = []string{
-		"https://www.google.com/search?q=",
-		"https://check-host.net/",
-		"https://www.facebook.com/",
-		"https://www.youtube.com/",
-		"https://www.ip138.com/",
-		"https://www.bing.com/search?q=",
-		"https://r.search.yahoo.com/",
-		"https://www.china.gov/index.html",
-		"https://vk.com/profile.php?redirect=",
-		"https://blog.csdn.net/",
-		"https://help.baidu.com/searchResult?keywords=",
-		"https://steamcommunity.com/market/search?q=",
-		"https://wx.zsxq.com/",
-		"https://www.android-ide.com/tutorial_androidapp.html",
-		"https://www.baidu.com/s?ie=utf-8&wd=AGDDoS%2FAGDDoS",
-		"https://www.baidu.com/s?ie=utf-8&wd=github.com%2FAGDDoS%2FAGDDoS",
-		// Don't forget to add ourselves!
-		"https://github.com/AGDDoS/AGDDoS",
-	}
 )
 
 // Main Function
 func main() {
 	defaultTargetUrl := "https://kzkt.tianyuyun.com/static/h5_new_4.6.5.115/index.html"
-	if os.Args[1] == "--help" { //This argument was deprecated.
-		fmt.Println(HelpMsg)
-		os.Exit(0)
-	}
 	printWelcome()
 	// Parse Flags / 解析命令行参数
+	flag.BoolVar(&DisplayTotal, "u", false, "显示统计（未上线）")
 	flag.StringVar(&Method, "m", "GET", "DDoS Method(GET/POST/PUT/HEAD/...)")
 	flag.StringVar(&TargetUrl, "u", defaultTargetUrl, "Taget URL")
 	flag.IntVar(&ConcurrencyCount, "cc", 8000, "并发线程数量")
@@ -180,10 +121,6 @@ func printWelcome() {
 	time.Sleep(time.Second)
 }
 
-func Log(grindex int, i int, responseStatus string) {
-	fmt.Printf("[INFO][AGDDoS#%d/%d]%s \033[0m \n", grindex, i, responseStatus) // 默认 [INFO][AGDDoS#当前线程序号/第i次重复] (Get "https://1.1.1.1": dial tcp 1.1.1.1:443: i/o timeout)
-}
-
-func PrintError(grindex int, i int, responseStatus string) {
-	fmt.Printf("[Error][AGDDoS#%d/%d] \033[1;31;40m (%s) \033[0m \n", grindex, i, responseStatus) // 默认
+func printTotal(*string, int) {
+	// TODO: #11 add Total print.
 }
