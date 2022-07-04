e952d2ed63e53f570630bfe389861aa486aa979e
甜力怕 <pig2333minecraft@yeah.net>
2022-07-04 21:14:06 +0800

    add zip.sh

---
+++ scripts/zip.sh
@@ -0,0 +1,7 @@
+zip -q -r AGDDoS-linux-amd64.zip ./build/linux/AGDDoS-linux-amd64
+zip -q -r AGDDoS-linux-arm64.zip ./build/linux/AGDDoS-linux-arm64
+zip -q -r AGDDoS-linux-arm.zip ./build/linux/AGDDoS-linux-arm
+zip -q -r AGDDoS-darwin.zip ./build/darwin/*
+zip -q -r AGDDoS-win64-amd64.zip ./build/windows/AGDDoS-amd64.exe
+zip -q -r AGDDoS-win64-arm64.zip ./build/windows/AGDDoS-arm64.exe
+zip -q -r AGDDoS-win32.zip ./build/windows/AGDDoS-x86.exe
