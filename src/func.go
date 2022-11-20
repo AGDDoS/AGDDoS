fd7e01557c338a40cff97f04794fc31259c4060e
甜力怕 <pig2333minecraft@yeah.net>
2022-11-20 09:07:35 +0800

    style ;modity

--- src/func.go
+++ src/func.go
@@ -34,17 +34,15 @@ func DoHttpRequest() (*string, error) {
 	if err != nil {
 		return nil, err
 	}
-	// Make yourself don't look like a robot
-	request.Header.Set("Accept", "*/*")                        // 接受所有
-	request.Header.Set("Accept-encoding", "gzip, deflate, br") // 声明浏览器支持的编码类型
-	request.Header.Set("Accept-language", "en-US,zh-TW;q=0.8") // 接受网页语言
-	request.Header.Set("Bypass", "true")
-	request.Header.Set("Cookies", genRandstr(4)+"="+genRandstr(16))
-	request.Header.Set("Referrer", Refs[rand.Intn(len(Refs))])                       // 生成伪来源页面的地址
-	request.Header.Set("User-Agent", UserAgents[rand.Intn(len(UserAgents))])         // 生成伪UA
+	request.Header.Set("Accept", "*/*")                                              // Accept All
+	request.Header.Set("Accept-encoding", "gzip, deflate, br")                       // 声明浏览器支持的编码类型
+	request.Header.Set("Accept-language", "en-US,zh-TW;q=0.8")                       // 接受网页语言
+	request.Header.Set("Bypass", "true")                                             // Bypass CF
+	request.Header.Set("Cookies", genRandstr(4)+"="+genRandstr(16))                  // Fake Cokkies
+	request.Header.Set("Referrer", Refs[rand.Intn(len(Refs))])                       // Fake Ref
+	request.Header.Set("User-Agent", UserAgents[rand.Intn(len(UserAgents))])         // Fake UA
 	request.Header.Set("X-Forward-For", genIpaddr()+","+genIpaddr()+","+genIpaddr()) // 多 层 代 理
 	request.Header.Set("X-Real-IP", genIpaddr())                                     // 多 层 代 理
-
 	response, err := DDosHttpClient.Do(request)
 	if err != nil {
 		return nil, err
